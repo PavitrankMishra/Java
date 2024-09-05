c55d910ebd609d17f3c53c51505500a126727eaa
Pavitrank <pavitrank2002@gmail.com>
2024-09-05 23:44:00 +0530

    Implemented stack using array

--- src/com/company/Stacks/st1.java
+++ src/com/company/Stacks/st1.java
@@ -1,37 +1,57 @@
 package com.company.Stacks;
 
-//Implementation of stack
-
+// Implementation of stack
 public class st1 {
-    int[] stack = new int[5];
-    int top = 0;
+    int st1[] = new int[10];
+    int max = 10;
+    int top = -1;  // Initialize top to -1
 
-    void push(int data) {
-        stack[top] = data;
+    void push(int val) {
+        if (top >= max - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+
         top++;
+        st1[top] = val;
+        System.out.println("The pushed value is: " + st1[top]);
     }
 
     int pop() {
-        top--;
-        int data = stack[top];
-        stack[top] = 0;
-        return data;
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return -1;  // Return a value indicating underflow
+        }
+        int x = st1[top--];
+        System.out.println("The popped value is: " + x);
+        return x;
     }
 
     int peek() {
-        return stack[top];
-    }
-
-    int getSize() {
-        return top;
+        if (top < 0) {
+            System.out.println("Stack is empty");
+            return -1;  // Return a value indicating the stack is empty
+        } else {
+            return st1[top];
+        }
     }
 
     boolean isEmpty() {
-        return top<=0;
+        return (top < 0);
     }
+
+    int size() {
+        return top + 1;  // Return the number of elements in the stack
+    }
+
     void show() {
-        for (int n : stack) {
-            System.out.print(n + " ");
+        if (top >= 0) {
+            for (int i = 0; i <= top; i++) {
+                System.out.print(st1[i] + " ");
+            }
+            System.out.println();  // Print a new line after showing the stack
+        } else {
+            System.out.println("Stack is empty");
         }
     }
 
@@ -40,19 +60,28 @@ public class st1 {
         s.push(1);
         s.push(2);
         s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        s.push(7);
 
         s.show();
 
         System.out.println();
-        System.out.println(s.pop());
+        System.out.println("Popped value: " + s.pop());
 
         s.show();
 
+        System.out.println();
         System.out.println("The peek element is: " + s.peek());
 
         s.push(29);
-        System.out.println("The size of the stack is: " + s.getSize());
+        System.out.println("The size of the stack is: " + s.size());
 
         System.out.println("Is the stack empty: " + s.isEmpty());
+        s.show();
+
+        System.out.println();
+        System.out.println("Final size of the stack: " + s.size());
     }
 }
